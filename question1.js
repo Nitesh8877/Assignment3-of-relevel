39d7b1df2b195b1e286cfa05e322c5ff6c0f9ae0
Nitesh Kumar <82534120+Nitesh8877@users.noreply.github.com>
2022-04-02 01:51:27 +0530

    Add files via upload

---
+++ question1.js
@@ -0,0 +1,10 @@
+
+
+function astroguru(children,spouses,location,job){
+
+    console.log("You will be a "+job+" in "+location+" and married to "+
+    spouses+" with "+children+" children");
+}
+
+astroguru(2,"Nitesh","Pune","Full Stack Developer");
+
---
+++ question2.js
@@ -0,0 +1,9 @@
+function twoNumber(num1,num2){
+
+    var result=(num1>num2)?num1:num2;
+    return result;
+}
+
+var large=twoNumber(34,23);
+
+console.log(large);
